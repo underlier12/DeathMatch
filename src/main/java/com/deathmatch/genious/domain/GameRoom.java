4f33f806985e99ab3037d7f9f67edb5a5836b0ea
underlier12 <underlier12@gmail.com>
2020-02-27 14:45:12 +0900

    refactor: Modify classes along with handling messages
    Preclude handling functions on GameRoom, GameRoomService. Handling functions only operate at Handler and HandlerService.

--- src/main/java/com/deathmatch/genious/domain/GameRoom.java
+++ src/main/java/com/deathmatch/genious/domain/GameRoom.java
@@ -5,9 +5,6 @@ import java.util.Set;
 
 import org.springframework.web.socket.WebSocketSession;
 
-import com.deathmatch.genious.service.GameRoomService;
-import com.deathmatch.genious.service.UnionService;
-
 import lombok.Builder;
 import lombok.Getter;
 
@@ -22,84 +19,5 @@ public class GameRoom {
         this.roomId = roomId;
         this.name = name;
     }
- 
-//    // 세션 연결
-//    public void connectionEstablish(WebSocketSession session, GameDTO gameDTO, GameRoomService gameRoomService) {
-//    	System.out.println("=========== Enter ChatRoom =========");
-//    	System.out.println("=========== connectionEstablish(WebSocketSession, ChatMessage, ChatService) =========");
-//    	System.out.println();
-//    	
-//        sessions.add(session);
-//        gameDTO.setMessage(gameDTO.getSender() + "님이 입장했습니다.");
-//    	sendMessage(gameDTO, gameRoomService);
-//    	
-//    }
-    
-    public void handleActions(WebSocketSession session, GameDTO gameDTO, 
-    		GameRoomService gameRoomService, UnionService unionService) {
-    	
-    	System.out.println("=========== Enter ChatRoom =========");
-    	System.out.println("=========== handleActions(WebSocketSession, ChatMessage, ChatService) =========");
-    	System.out.println();
-    	
-    	if (gameDTO.getType().equals(GameDTO.MessageType.JOIN)) {
-    		// JOIN
-		    sessions.add(session);
-		    gameDTO.setMessage(gameDTO.getSender() + "님이 입장했습니다.");
-		    sendMessage(gameDTO, gameRoomService);
-		    
-    	} else if (gameDTO.getType().equals(GameDTO.MessageType.TALK)) {
-    		// TALK
-    		sendMessage(gameDTO, gameRoomService);
-    		
-    	} else if (gameDTO.getType().equals(GameDTO.MessageType.READY)) {
-    		// READY
-    		gameDTO.setMessage(gameDTO.getSender() + "님이 준비하셨습니다.");
-    		unionService.readyCheck(sessions, gameDTO);
-    		
-    	} else if (gameDTO.getType().equals(GameDTO.MessageType.SCORE)) {
-    		// SCORE
-    		countScore(gameDTO, unionService);
-    		
-    	} else if (gameDTO.getType().equals(GameDTO.MessageType.UNI)) {
-    		// UNI
-    		uniCheck(gameDTO, unionService);
-//    		unionService.uniCheck(session, gameDTO);
-    		
-    	} else if (gameDTO.getType().equals(GameDTO.MessageType.ON)) {
-    		// ON
-    		gameDTO.setMessage(gameDTO.getSender() + "님이 '결'을 외치셨습니다.");
-    		unionService.onCheck(session, gameDTO);
-    		
-    	} else {
-    		// OUT
-    		
-    	}
-    	
-    }
 
-    public <T> void sendMessage(T message, GameRoomService gameRoomService) {
-    	
-    	System.out.println("=========== Enter ChatRoom =========");
-    	System.out.println("=========== sendMessage(T, ChatService) =========");
-    	System.out.println();
-    	
-        sessions.parallelStream().forEach(session -> gameRoomService.sendMessage(session, message));
-    }
-    
-    public <T> void countScore(T message, UnionService unionService) {
-    	System.out.println("=========== Enter ChatRoom =========");
-    	System.out.println("=========== countScore(T, ChatService) =========");
-    	System.out.println();
-    	
-    	sessions.parallelStream().forEach(session -> unionService.countScore(session, message));
-    }
-    
-    public <T> void uniCheck(T message, UnionService unionService) {
-    	System.out.println("=========== Enter ChatRoom =========");
-    	System.out.println("=========== uniCheck(T, ChatService) =========");
-    	System.out.println();
-    	
-    	sessions.parallelStream().forEach(session -> unionService.countScore(session, message));
-    }
 }
--- src/main/java/com/deathmatch/genious/service/UnionService.java
+++ src/main/java/com/deathmatch/genious/service/UnionService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import com.deathmatch.genious.domain.GameDTO;
+import com.deathmatch.genious.domain.GameRoom;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -18,6 +20,62 @@ public class UnionService {
 
 	private ObjectMapper objectMapper = new ObjectMapper();
 
+	public void handleActions(WebSocketSession session, GameDTO gameDTO, GameRoom gameRoom) {
+		
+		Set<WebSocketSession> sessions = gameRoom.getSessions();
+		
+		if (gameDTO.getType().equals(GameDTO.MessageType.JOIN)) {
+			// JOIN
+			sessions.add(session);
+			gameDTO.setMessage(gameDTO.getSender() + "님이 입장했습니다.");
+			sendMessageAll(sessions, gameDTO);
+			
+		} else if (gameDTO.getType().equals(GameDTO.MessageType.TALK)) {
+    		// TALK
+			sendMessageAll(sessions, gameDTO);
+    		
+    	} else if (gameDTO.getType().equals(GameDTO.MessageType.READY)) {
+    		// READY
+    		gameDTO.setMessage(gameDTO.getSender() + "님이 준비하셨습니다.");
+    		unionService.readyCheck(sessions, gameDTO);
+    		
+    	} else if (gameDTO.getType().equals(GameDTO.MessageType.SCORE)) {
+    		// SCORE
+    		countScore(gameDTO, unionService);
+    		
+    	} else if (gameDTO.getType().equals(GameDTO.MessageType.UNI)) {
+    		// UNI
+    		uniCheck(gameDTO, unionService);
+//    		unionService.uniCheck(session, gameDTO);
+    		
+    	} else if (gameDTO.getType().equals(GameDTO.MessageType.ON)) {
+    		// ON
+    		gameDTO.setMessage(gameDTO.getSender() + "님이 '결'을 외치셨습니다.");
+    		unionService.onCheck(session, gameDTO);
+    		
+    	} else {
+    		// OUT
+    		
+    	}
+	}
+	
+	public <T> void sendMessageAll(Set<WebSocketSession> sessions, T message){
+		sessions.parallelStream().forEach(session -> sendMessage(session, message));
+
+	}
+	
+	public <T> void sendMessage(WebSocketSession session, T message) {
+		
+		try {
+			session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public <T> void readyCheck(Set<WebSocketSession> sessions, T message) {
 		// TODO
 		System.out.println("=========== Enter UnionService =========");
@@ -71,31 +129,4 @@ public class UnionService {
 		}
 	}
 	
-	public <T> void sendMessage(WebSocketSession session, T message) {
-		
-		System.out.println("=========== Enter UnionService =========");
-		System.out.println("=========== sendMessage(WebSocketSession, T) =========");
-		System.out.println();
-		
-		try {
-			session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	
-//	public void handleActionOthers(WebSocketSession session, GameDTO gameDTO) {
-//		
-//		System.out.println("=========== Enter UnionService =========");
-//    	System.out.println("=========== sendMessage(WebSocketSession, T) =========");
-//    	System.out.println();
-//    	
-//    	
-//    	
-//	}
-//	
 }
