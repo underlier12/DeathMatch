de6c97b8570741f6f827a6b4d8b2cc8f4bd9831c
underlier12 <underlier12@gmail.com>
2020-04-22 23:55:28 +0900

    feat: Add UnionLoadingDAO's methods

--- src/main/java/com/deathmatch/genius/dao/UnionLoadingDAO.java
+++ src/main/java/com/deathmatch/genius/dao/UnionLoadingDAO.java
@@ -1,6 +1,14 @@
 package com.deathmatch.genius.dao;
 
-public class UnionLoadingDAO {
+import java.util.List;
 
+import com.deathmatch.genius.domain.UnionLoadingDTO;
+
+public interface UnionLoadingDAO {
+
+	// select union problem (only card names)
+	public List<String> selectUnionProblemCardNames(UnionLoadingDTO loadingDTO);
 	
+	// select union answer sheet (only submitted)
+	public List<String> selectUnionAnswerSheet(UnionLoadingDTO loadingDTO);
 }
