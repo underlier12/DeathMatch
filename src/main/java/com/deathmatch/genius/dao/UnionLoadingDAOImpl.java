a7d3c1cc9df560d09cf3383b9a5a006bfd65f567
underlier12 <underlier12@gmail.com>
2020-04-24 23:52:51 +0900

    feat: Implement LoadingDAO and add LoadingMapper

---
+++ src/main/java/com/deathmatch/genius/dao/UnionLoadingDAOImpl.java
@@ -0,0 +1,24 @@
+package com.deathmatch.genius.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.deathmatch.genius.domain.UnionLoadingDTO;
+
+@Repository
+public class UnionLoadingDAOImpl implements UnionLoadingDAO {
+
+	@Override
+	public List<String> selectUnionProblemCardNames(UnionLoadingDTO loadingDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> selectUnionAnswerSheet(UnionLoadingDTO loadingDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
