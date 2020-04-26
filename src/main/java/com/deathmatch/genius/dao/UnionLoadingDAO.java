fd946dc60bc8960948b4184ba749ee64a6345414
underlier12 <underlier12@gmail.com>
2020-04-26 13:55:45 +0900

    feat: Add method for select problem and answer sheet for loading

--- src/main/java/com/deathmatch/genius/dao/UnionLoadingDAO.java
+++ src/main/java/com/deathmatch/genius/dao/UnionLoadingDAO.java
@@ -2,13 +2,13 @@ package com.deathmatch.genius.dao;
 
 import java.util.List;
 
-import com.deathmatch.genius.domain.UnionLoadingDTO;
+import com.deathmatch.genius.domain.UnionDatabaseDTO;
 
 public interface UnionLoadingDAO {
 
 	// select union problem (only card names)
-	public List<String> selectUnionProblemCardNames(UnionLoadingDTO loadingDTO);
+	public List<String> selectUnionProblemCardNames(UnionDatabaseDTO dbDTO);
 	
 	// select union answer sheet (only submitted)
-	public List<String> selectUnionAnswerSheet(UnionLoadingDTO loadingDTO);
+	public List<String> selectUnionAnswerSheet(UnionDatabaseDTO dbDTO);
 }
--- src/main/java/com/deathmatch/genius/dao/UnionLoadingDAOImpl.java
+++ src/main/java/com/deathmatch/genius/dao/UnionLoadingDAOImpl.java
@@ -1,24 +1,50 @@
 package com.deathmatch.genius.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
-import com.deathmatch.genius.domain.UnionLoadingDTO;
+import com.deathmatch.genius.domain.UnionDatabaseDTO;
 
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 @Repository
 public class UnionLoadingDAOImpl implements UnionLoadingDAO {
-
-	@Override
-	public List<String> selectUnionProblemCardNames(UnionLoadingDTO loadingDTO) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private String namespace = "com.deathmatch.genius.mapper.UnionLoadingMapper";
+	private SqlSession sqlSession;
+	
+	public UnionLoadingDAOImpl(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
 	}
 
 	@Override
-	public List<String> selectUnionAnswerSheet(UnionLoadingDTO loadingDTO) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> selectUnionProblemCardNames(UnionDatabaseDTO dbDTO) {
+		List<String> problem = new ArrayList<>();
+		
+		problem = sqlSession.selectList(namespace + ".selectProblem");
+//		for(int i = 0; i < 9; i++) {
+//			dbDTO.setIdx(i);
+//			String cardName
+//					= sqlSession.selectOne(namespace + ".selectProblemEach");
+//			problem.add(cardName);
+//		}
+		log.info("problem : " + problem.toString());
+		
+		return problem;
+	}
+
+	@Override
+	public List<String> selectUnionAnswerSheet(UnionDatabaseDTO dbDTO) {
+		List<String> answerSheet = new ArrayList<>();
+		
+		answerSheet = sqlSession.selectList(namespace + ".selectAnswerSheet");
+		log.info("answerSheet : " + answerSheet.toString());
+		
+		return answerSheet;
 	}
 
 }
