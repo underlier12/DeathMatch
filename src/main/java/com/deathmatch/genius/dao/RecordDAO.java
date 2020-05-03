dcd488675a0ed21067fe32f2027bb36edd352a65
underlier12 <underlier12@gmail.com>
2020-05-03 16:57:54 +0900

    feat: fill out RecordDAO

--- src/main/java/com/deathmatch/genius/dao/RecordDAO.java
+++ src/main/java/com/deathmatch/genius/dao/RecordDAO.java
@@ -1,5 +1,20 @@
 package com.deathmatch.genius.dao;
 
+import java.util.List;
+
+import com.deathmatch.genius.domain.RecordDTO;
+
 public interface RecordDAO {
 
+	// insert record of battle
+	public void insertHistory(RecordDTO recordDTO);
+	
+	// select all record by user id
+	public List<RecordDTO> selectAllRecord();
+	
+	// select record by game type
+	public List<RecordDTO> selectRecordByGameId(String gameType);
+	
+	// select record of opponent related to gameId
+	public RecordDTO selectOpponentRecord(RecordDTO recordDTO);
 }
