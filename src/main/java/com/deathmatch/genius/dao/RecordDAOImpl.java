b8bfe1d3d7747f9f8c9ea5df2973a1f11c2fda61
underlier12 <underlier12@gmail.com>
2020-05-05 14:22:41 +0900

    feat: Add method insertHistory at RecordDAO

--- src/main/java/com/deathmatch/genius/dao/RecordDAOImpl.java
+++ src/main/java/com/deathmatch/genius/dao/RecordDAOImpl.java
@@ -1,5 +1,41 @@
 package com.deathmatch.genius.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.deathmatch.genius.domain.RecordDTO;
+
 public class RecordDAOImpl implements RecordDAO {
+	
+	private String namespace = "com.deathmatch.genius.mapper.RecordMapper";
+	private SqlSession sqlSession;
+	
+	public RecordDAOImpl(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	@Override
+	public void insertHistory(RecordDTO recordDTO) {
+		sqlSession.insert(namespace + ".insertHistory", recordDTO);
+	}
+
+	@Override
+	public List<RecordDTO> selectAllRecord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RecordDTO> selectRecordByGameId(String gameType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RecordDTO selectOpponentRecord(RecordDTO recordDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
