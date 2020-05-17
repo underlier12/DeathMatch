3590e430d5f59314d1a52f94b25fef0a1c06bd9f
underlier12 <underlier12@gmail.com>
2020-05-17 15:43:28 +0900

    refactor: Change parameters in RecordController

--- src/main/java/com/deathmatch/genius/controller/RecordController.java
+++ src/main/java/com/deathmatch/genius/controller/RecordController.java
@@ -1,10 +1,13 @@
 package com.deathmatch.genius.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.deathmatch.genius.domain.UserDTO;
 import com.deathmatch.genius.service.RecordService;
 import com.deathmatch.genius.util.Criteria;
 import com.deathmatch.genius.util.PageDTO;
@@ -19,11 +22,13 @@ public class RecordController {
 		private final RecordService recordService;
 		
 		@GetMapping
-		public String showHistory(Criteria criteria, Model model) {
+		public String showHistory(Criteria criteria, Model model, HttpSession httpSession) {
+			UserDTO currentDTO = (UserDTO) httpSession.getAttribute("login");
+			String userId = currentDTO.getUserId();
 			
-			PageDTO pageDTO = new PageDTO(criteria, recordService.countRecord());
+			PageDTO pageDTO = new PageDTO(criteria, recordService.countRecord(userId));
 			
-			model.addAttribute("record", recordService.findRecordList(criteria));
+			model.addAttribute("record", recordService.findRecordList(criteria, userId));
 			model.addAttribute("pageMaker", pageDTO);
 			
 			return "record/record-history";
