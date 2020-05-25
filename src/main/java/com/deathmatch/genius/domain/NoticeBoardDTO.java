0401db0341b17ea2133682f09de4710115acff72
underlier12 <underlier12@gmail.com>
2020-05-25 11:27:37 +0900

    feat: Add NoticeBoardDTO

---
+++ src/main/java/com/deathmatch/genius/domain/NoticeBoardDTO.java
@@ -0,0 +1,40 @@
+package com.deathmatch.genius.domain;
+
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NoticeBoardDTO {
+	
+	private int bno;
+	private String userId;
+	private String title;
+	private String content;
+	private LocalDate regdate;
+	private int hit;
+//	private Integer ref; // 그룹 번호
+//	private int step; // 순서
+//	private int depth; // 단계
+	private int pw; // 비밀 번호
+	
+	@Builder
+	public NoticeBoardDTO(int bno,String userId,String title,String content,LocalDate regdate,
+			int hit,Integer ref,int step,int depth,int pw){
+		this.bno = bno;
+		this.userId = userId;
+		this.title = title;
+		this.content = content;
+		this.regdate = regdate;
+		this.hit = hit;
+//		this.ref = ref;
+//		this.step = step;
+//		this.depth = depth;
+		this.pw = pw;
+	}
+}
