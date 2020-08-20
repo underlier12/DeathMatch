7152d36b65000cc2c9eeea4e59b5019e13a734fb
Guen ho Hong <hogu8159@naver.com>
2020-08-20 22:19:12 +0900

    restore

--- src/main/java/com/deathmatch/genius/controller/HomeController.java
+++ src/main/java/com/deathmatch/genius/controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-
 	@GetMapping
 	public String home() {
+		System.out.println("Homse Controller");
 		return "index";
 	}
 
